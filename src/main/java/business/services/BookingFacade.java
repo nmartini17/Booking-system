a928368319b117a83d645548113d8542ae4670c7
Nicolai Martini <cph-nm172@cphbusiness.dk>
2021-07-19 14:48:06 +0200

    US-5 done

--- src/main/java/business/services/BookingFacade.java
+++ src/main/java/business/services/BookingFacade.java
@@ -15,12 +15,6 @@ public class BookingFacade {
         this.bookingMapper = new BookingMapper(database);
     }
 
-    /*public Booking insertIntoBooking(int id, int days, String comment, boolean booking_status, int item_id) throws UserException {
-        Booking booking = new Booking(id, days, comment, booking_status, item_id);
-        booking = bookingMapper.insertIntoBooking(booking);
-        return booking;
-
-    }*/
     public List<Booking> getAllBookings() throws UserException {
         return bookingMapper.getAllBookings();
     }
